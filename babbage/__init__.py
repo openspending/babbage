59455f391f1be620bf563f9a8cd7ffc611c72ca5
Vitor Baptista <vitor@vitorbaptista.com>
2017-08-09 17:50:00 +0100

    Bump version to 0.3.0

--- babbage/__init__.py
+++ babbage/__init__.py
@@ -1,6 +1,6 @@
 """ Babbage, an OLAP-like, light-weight database analytical engine. """
 
-__version__ = "0.2.3"
+__version__ = "0.3.0"
 
 from babbage.manager import CubeManager, JSONCubeManager  # noqa
 from babbage.api import configure_api  # noqa
