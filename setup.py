c8ee1b116c82f2d21aeed1f8a57765f85af59d1c
Vitor Baptista <vitor@vitorbaptista.com>
2017-08-10 14:54:55 +0100

    Support Python versions 2.7, 3.4, 3.5 and 3.6

--- setup.py
+++ setup.py
@@ -16,7 +16,9 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3'
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
     ],
     keywords='sql sqlalchemy olap cubes analytics',
     author='Friedrich Lindenberg',
